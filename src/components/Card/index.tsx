7cf5d8ffd8dedd3f715309e2e1825e4eed504a4f
regisdantas <dantas.regis.m@gmail.com>
2022-11-02 20:19:59 -0300

    Added add button, cards and remove capability

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { CardContainer } from './styles';
+import { FiTrash2 } from 'react-icons/fi';
+
+interface ICardProps {
+  id: string;
+  content: string;
+  onDeleteCard: any;
+  onChangeContent: any;
+}
+
+const Card: React.FC<ICardProps> = ({
+  id,
+  content,
+  onDeleteCard,
+  onChangeContent
+}: ICardProps) => {
+  const textRef = React.useRef<HTMLSpanElement>(null);
+  return (
+    <CardContainer>
+      <div className="ContentContainer">
+        <header>
+          <strong>{id}</strong>
+            <FiTrash2 onClick={e => onDeleteCard(id)}></FiTrash2>
+        </header>
+        <span
+          ref={textRef}
+          role="textbox"
+          contentEditable
+          data-placeholder='What are you grateful for this day?'
+          onBlur={e => onChangeContent(id, e.currentTarget.innerText)}
+          suppressContentEditableWarning={true}
+        >
+          {content}
+        </span>
+      </div>
+    </CardContainer>
+  );
+};
+
+export default Card;
---
+++ src/components/Card/styles.ts
@@ -0,0 +1,90 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  font-size: 36px;
+  color: #3a3a3a;
+`;
+
+export const CardContainer = styled.div`
+  max-width: 400px;
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  padding: 0px;
+  margin: 10px 0px 0px 0px;
+
+  background-color: #eee;
+  border-radius: 5px;
+  border-left: 3px dashed lightgray;
+
+  div {
+    margin-left: 10px;
+    width: 100%;
+  }
+
+  .ContentContainer {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    width: 100%;
+    border-radius: 5px 5px 0px 0px;
+    margin: 0;
+    background: #fff;
+
+    header {
+      width: 100%;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+
+      svg {
+        margin: 10px;
+        cursor: pointer;
+      }
+    }
+    strong {
+      width: 100%;
+      padding: 10px;
+    }
+
+    span {
+      border: 0px;
+      padding: 10px;
+      width: 100%;
+      text-align: justify;
+
+      max-height: 200px;
+      overflow: hidden;
+    }
+
+    span:empty::before {
+      content: attr(data-placeholder);
+      display: inline-block;
+      color: lightgray;
+    }
+
+    span:focus {
+      outline: none;
+    }
+
+    .ActionContainer {
+      width: 100%;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-around;
+      margin: 4px;
+      p {
+        cursor: pointer;
+        display: flex;
+        align-items: center;
+        * {
+          margin-right: 5px;
+        }
+      }
+    }
+  }
+`;
