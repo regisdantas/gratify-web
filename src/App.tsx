b9e2938c2d209f6c4d9af9c559ed436362020bd6
regisdantas <dantas.regis.m@gmail.com>
2022-11-22 19:24:36 -0300

    Adding login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
 import { GlobalStyles } from './styles/global';
-import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
 
 function App() {
+
   return (
     <>
-      <Dashboard />
+      <Login />
       <GlobalStyles />
     </>
   );
